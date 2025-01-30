557bf72bf1d40414d93d33dcab6e61f2e4e9cc8b
ESDDVUNIR <ernestosalvador.ramirezBE9@comunidadunir.net>
2025-01-30 16:54:08 -0600

    Main Game Top down RPG
    Juego base de Top Down RPG

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float inputH;
+    private float inputV;
+    private bool moviendo;
+    private Vector3 puntoDestino;
+    private Vector3 ultimoInput;
+    private Vector3 puntoInteraccion;
+    private Collider2D colliderDelante; //Indica el collider que tenemos delante.
+    private Animator anim;
+
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float radioInteraccion;
+    [SerializeField] private LayerMask queEsColisionable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LecturaInputs();
+
+        MovimientoyAnimaciones();
+    }
+
+    private void MovimientoyAnimaciones()
+    {
+        //Ejecuto movimiento solo si estoy en una casilla y solo si hay input
+        if (!moviendo && (inputH != 0 || inputV != 0)) //!= ==false
+        {
+            anim.SetBool("andando", true);
+            anim.SetFloat("InputH", inputH);
+            anim.SetFloat("InputV", inputV);
+            //Actualizo cual fue mi ultimo input, cual va a ser mi punto destino y cual es mi punto interaccion.
+            ultimoInput = new Vector3(inputH, inputV, 0);
+            puntoDestino = transform.position + ultimoInput;
+            puntoInteraccion = puntoDestino;
+
+            colliderDelante = LanzarCheck();
+
+            if (!colliderDelante)
+            {
+                StartCoroutine(Mover());
+            }
+        }
+        else if (inputH == 0 && inputV == 0)
+        {
+            anim.SetBool("andando", false);
+        }
+    }
+
+    private void LecturaInputs()
+    {
+        if (inputV == 0)
+        {
+            inputH = Input.GetAxisRaw("Horizontal");
+        }
+        if (inputH == 0)
+        {
+            inputV = Input.GetAxisRaw("Vertical");
+        }
+    }
+
+    IEnumerator Mover()
+    {
+        moviendo = true;
+        while (transform.position != puntoDestino)
+        {
+        transform.position = Vector3.MoveTowards(transform.position, puntoDestino, velocidadMovimiento * Time.deltaTime);
+        yield return null;
+        }
+        //Ante un nuevo destino, necesito refrescar de nuevo punto interaccion.
+        puntoInteraccion = transform.position + ultimoInput;
+        moviendo = false;
+    }
+    private Collider2D LanzarCheck()
+    {
+        return Physics2D.OverlapCircle(puntoInteraccion, radioInteraccion, queEsColisionable);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(puntoInteraccion, radioInteraccion);
+    }
+}
